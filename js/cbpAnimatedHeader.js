74f60d08f92785f39fd1163c2fb15b54743f69ea
unknown <TT--@users.noreply.github.com>
2017-01-18 19:13:31 -0500

    resolves y7kim/agency-jekyll-theme#51 - when scrolling down navbar shrinks sooner to eliminate overlap between navbar and header text

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 100;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
