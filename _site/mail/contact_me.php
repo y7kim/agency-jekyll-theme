9738c5b962cfd894383a09ee0ca9829bc40d7d32
nyampire <nyampire@gmail.com>
2018-04-19 15:22:49 +0900

    disable phone requirement

--- _site/mail/contact_me.php
+++ _site/mail/contact_me.php
@@ -2,7 +2,7 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
+//   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
