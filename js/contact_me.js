cca717e97e4f4ebd8dd20040a9149fb77dbd13b4
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-04-15 15:07:16 -0500

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/mqkdjpzy",
                 type: "POST",
                 data: {
                     name: name,
