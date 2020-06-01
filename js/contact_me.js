160ad0502a419edbd9bbbe6098385b71f1855319
Cristianetaniguti <chaytaniguti@gmail.com>
2020-06-01 00:14:56 -0300

    up

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: url('{{ site.baseurl }}/mail/contact_me.php'),
+                url: '{{ site.baseurl }}/mail/contact_me.php',
                 type: "POST",
                 data: {
                     name: name,
