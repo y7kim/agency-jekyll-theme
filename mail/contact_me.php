bb558283d8feb8fa83978bb44d3efa5f1cb5da95
Cristianetaniguti <chaytaniguti@gmail.com>
2020-05-31 23:45:44 -0300

    remove phone

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,13 +11,12 @@ if(empty($_POST['name'])  		||
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'chaytaniguti@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato - site RUTA:  $name";
-$email_body = "Você recebeu uma nova mensagem do website da Coletiva RUTA.\n\n"."Aqui estão os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+$email_body = "Você recebeu uma nova mensagem do website da Coletiva RUTA.\n\n"."Aqui estão os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nMensagem:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Responda para: $email_address";
 mail($to,$email_subject,$email_body,$headers);
